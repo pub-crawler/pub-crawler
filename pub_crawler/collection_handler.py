915db045f334ea3377731f1c7a8d8e0a5d22375c
Evan Prodromou <evanp@gatech.edu>
2026-06-15 15:37:10 -0400

    refactor: bulk graph update in CollectionHandler

--- pub_crawler/collection_handler.py
+++ pub_crawler/collection_handler.py
@@ -17,47 +17,42 @@ class CollectionHandler(Handler):
         direction = job["direction"]
         depth = job["depth"]
         await self.graph.ensure_node(owner_id)
+        props = {}
+        json = {}
         try:
             json = await self.client.get(collection_id)
         except httpx.HTTPStatusError as err:
-            await self.graph.set_node_property(
-                owner_id, f"{direction}_http_status", err.response.status_code
-            )
-            return
-        await self.graph.set_node_property(owner_id, f"{direction}_http_status", 200)
-        await self._set_prop(owner_id, json, f"{direction}_count", "totalItems")
-        if depth < self.max_depth:
-            first = json.get("first", None)
-            if first:
-                await self.graph.set_node_property(
-                    owner_id, f"{direction}_members_shared", True
-                )
-                await self.dispatcher.enqueue(
-                    {
-                        "job_type": "page",
-                        "page_id": first,
-                        "owner_id": owner_id,
-                        "direction": direction,
-                        "depth": depth,
-                    }
-                )
-            else:
-                items = json.get("items", json.get("orderedItems", None))
-                if items:
-                    await self.graph.set_node_property(
-                        owner_id, f"{direction}_members_shared", True
-                    )
-                    await handle_items(
-                        self.graph, self.dispatcher, items, owner_id, direction, depth
+            props[f"{direction}_http_status"] = err.response.status_code
+
+        if f"{direction}_http_status" not in props:
+            props[f"{direction}_http_status"] = 200
+            if "totalItems" in json and isinstance(json["totalItems"], int):
+                props[f"{direction}_count"] = json["totalItems"]
+
+            if depth < self.max_depth:
+                first = json.get("first", None)
+                if first:
+                    props[f"{direction}_members_shared"] = True
+                    await self.dispatcher.enqueue(
+                        {
+                            "job_type": "page",
+                            "page_id": first,
+                            "owner_id": owner_id,
+                            "direction": direction,
+                            "depth": depth,
+                        }
                     )
                 else:
-                    await self.graph.set_node_property(
-                        owner_id, f"{direction}_members_shared", False
-                    )
+                    items = json.get("items", json.get("orderedItems", None))
+                    if items:
+                        props[f"{direction}_members_shared"] = True
+                        await handle_items(
+                            self.graph, self.dispatcher, items, owner_id, direction, depth
+                        )
+                    else:
+                        props[f"{direction}_members_shared"] = False
+
+        await self.graph.set_node_properties(owner_id, props)
 
     def next_available(self, job):
         return self.client.next_available(job["collection_id"])
-
-    async def _set_prop(self, owner_id, json, prop, prop2):
-        if prop2 in json:
-            await self.graph.set_node_property(owner_id, prop, json.get(prop2))
