10fab6d0adab2d044e90c1cfc145a0e57b010c62
Evan Prodromou <evanp@gatech.edu>
2026-06-16 10:59:35 -0400

    fix: job_id() uses job_type, not type

--- pub_crawler/job_id.py
+++ pub_crawler/job_id.py
@@ -1,13 +1,13 @@
 def job_id(job):
   id = "<not a job>"
-  if isinstance(job, dict) and "type" in job:
-    type = job.get("type")
-    if type == "actor":
-      id = f"{type}:{job.get("actor_id")}"
-    elif type == "webfinger":
-      id = f"{type}:{job.get("webfinger")}"
-    elif type == "collection":
-      id = f"{type}:{job.get("collection_id")}"
-    elif type == "page":
-      id = f"{type}:{job.get("page_id")}"
+  if isinstance(job, dict) and "job_type" in job:
+    job_type = job.get("job_type")
+    if job_type == "actor":
+      id = f"{job_type}:{job.get("actor_id")}"
+    elif job_type == "webfinger":
+      id = f"{job_type}:{job.get("webfinger")}"
+    elif job_type == "collection":
+      id = f"{job_type}:{job.get("collection_id")}"
+    elif job_type == "page":
+      id = f"{job_type}:{job.get("page_id")}"
   return id
