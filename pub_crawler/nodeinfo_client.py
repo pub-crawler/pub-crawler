24d68c5b615f9702d65fd2483303dee756bf8037
Evan Prodromou <eprodromou3@gatech.edu>
2026-07-31 13:44:30 -0400

    chore: bump user-agent versions

--- pub_crawler/nodeinfo_client.py
+++ pub_crawler/nodeinfo_client.py
@@ -100,7 +100,7 @@ class NodeinfoClient:
         await self.burst.acquire(origin)
         url = f"{origin}/.well-known/nodeinfo"
         headers = {
-            "User-Agent": "crawler.pub/0.16.3 (https://crawler.pub/; evanp@gatech.edu)",
+            "User-Agent": "crawler.pub/0.16.4 (https://crawler.pub/; evanp@gatech.edu)",
             "Accept": "application/json;q=1.0",
         }
         res = await self._safe_get(url, headers)
